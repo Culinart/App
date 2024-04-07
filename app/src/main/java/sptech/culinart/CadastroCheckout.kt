97ae881fba6c8b674762da62a1edf1f28226e3e5
clarasalomaofaria <clara.faria@sptech>
2024-04-07 19:48:31 -0300

    Criação do front-end da página de CadastroCheckout

---
+++ app/src/main/java/sptech/culinart/CadastroCheckout.kt
@@ -0,0 +1,175 @@
+package sptech.culinart
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import sptech.culinart.ui.theme.CulinartTheme
+
+class CadastroCheckout : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CulinartTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TelaCadastroCheckout("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TelaCadastroCheckout(name: String, modifier: Modifier = Modifier) {
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color(249, 251, 251))
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(
+            painter = painterResource(id = R.mipmap.cadastroterceiraetapa),
+            contentDescription = "Cadastro etapa de checkout",
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+        )
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Card (
+            modifier = Modifier.fillMaxWidth(0.8f),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.White
+            ),
+            border = BorderStroke(2.dp, Color(228, 228, 228)),
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 4.dp
+            )
+        ){
+            Column(
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .align(Alignment.CenterHorizontally)
+                    .fillMaxHeight()
+                    .wrapContentHeight(align = Alignment.CenterVertically),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Spacer(modifier = Modifier.height(30.dp))
+
+                Text(
+                    "Checkout",
+                    style = TextStyle(
+                        Color(255, 159, 28),
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        fontSize = 22.sp
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(30.dp))
+
+                Row (
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text("+ X Refeições")
+                    Text("R$00,00")
+                }
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Canvas(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(1.dp)
+                ) {
+                    drawRoundRect(
+                        color = Color(140, 140, 140),
+                        cornerRadius = CornerRadius(0f)
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Row (
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text("Total")
+                    Text("R$00,00")
+                }
+
+                Spacer(modifier = Modifier.height(30.dp))
+                
+            }
+        }
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.width(250.dp),
+            shape = RoundedCornerShape(10.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(255, 159, 28),
+                contentColor = Color.White
+            )
+        ) {
+            Text("Finalizar Assinatura")
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+    CulinartTheme {
+        TelaCadastroCheckout("Android")
+    }
+}
