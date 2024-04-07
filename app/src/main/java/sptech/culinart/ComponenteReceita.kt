f3e8588d686d809f144f175bbdda6e938e1952d6
Calebe-Marques <calebe.reboucas@sptech.school>
2024-04-06 23:42:25 -0300

    fix: remoção de imports desnecessários no componente Header

--- app/src/main/java/sptech/culinart/ComponenteReceita.kt
+++ app/src/main/java/sptech/culinart/ComponenteReceita.kt
@@ -3,12 +3,16 @@ package sptech.culinart
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import sptech.culinart.ui.theme.CulinartTheme
 
 class ComponenteReceita : ComponentActivity() {
@@ -27,7 +31,11 @@ class ComponenteReceita : ComponentActivity() {
 
 @Composable
 fun ComponenteReceita(name: String, modifier: Modifier = Modifier){
-
+    Header()
+    Spacer(modifier = Modifier.height(25.dp))
+    Column {
+        
+    }
 }
 
 @Preview(showBackground = true)
