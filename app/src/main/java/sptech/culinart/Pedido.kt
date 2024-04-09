e7bfa05b8ddf36d4dfe8daed1d19477ed5a716d8
clarasalomaofaria <clara.faria@sptech>
2024-04-09 18:38:49 -0300

    Iniciando o front-end do card da data atual e as setas de navegação na página de Pedido

--- app/src/main/java/sptech/culinart/Pedido.kt
+++ app/src/main/java/sptech/culinart/Pedido.kt
@@ -4,29 +4,39 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -91,7 +101,73 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             )
         }
 
-        Spacer(modifier = Modifier.height(30.dp))
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Row (
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+
+            Image(painter = painterResource(id = R.mipmap.setaesquerda), contentDescription = "Seta Esquerda")
+
+            Card (modifier = modifier
+                .height(100.dp)
+                .width(70.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color(255, 241, 221)
+                ),
+                border = BorderStroke(2.dp, Color(255, 159, 28)),
+                elevation = CardDefaults.cardElevation(
+                    defaultElevation = 4.dp
+                )
+            ){
+                    Column( modifier = modifier
+                        .fillMaxHeight()
+                        .wrapContentHeight(align = Alignment.CenterVertically),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            "Sex",
+                            modifier = Modifier.fillMaxWidth(),
+                            style = TextStyle(
+                                Color(4, 93, 83),
+                                fontWeight = FontWeight.SemiBold,
+                                textAlign = TextAlign.Center,
+                                fontSize = 16.sp
+                            )
+                        )
+
+                        Text(
+                            "4",
+                            modifier = Modifier.fillMaxWidth(),
+                            style = TextStyle(
+                                Color(4, 93, 83),
+                                fontWeight = FontWeight.SemiBold,
+                                textAlign = TextAlign.Center,
+                                fontSize = 16.sp
+                            )
+                        )
+
+                        Text(
+                            "AGO",
+                            modifier = Modifier.fillMaxWidth(),
+                            style = TextStyle(
+                                Color(4, 93, 83),
+                                fontWeight = FontWeight.SemiBold,
+                                textAlign = TextAlign.Center,
+                                fontSize = 16.sp
+                            )
+                        )
+                    }
+
+            }
+
+            Image(painter = painterResource(id = R.mipmap.setadireita), contentDescription = "Seta Direita")
+
+        }
+
+        Spacer(modifier = Modifier.height(40.dp))
 
         Column (
             modifier = Modifier
