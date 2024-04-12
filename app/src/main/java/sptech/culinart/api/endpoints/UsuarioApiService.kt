9332e2210de174df02ecf02b0d5407126cb3c897
Lucas Akiama Brasiliano <lucas.brasiliano@yandeh.com.br>
2024-04-11 21:58:56 -0300

    criacao data pack

--- app/src/main/java/sptech/culinart/api/endpoints/UsuarioApiService.kt
+++ app/src/main/java/sptech/culinart/api/endpoints/UsuarioApiService.kt
@@ -1,9 +1,16 @@
 package sptech.culinart.api.endpoints
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
+
 interface UsuarioApiService {
 
     // Define o método GET para buscar usuários
     @GET("/usuarios")
     fun getUsuarios(): Call<List<UsuarioApiService>>
+
+    // Define o método POST para o login de usuário
+    @POST("/login")
+    fun login(@Body credenciais: CredenciaisLogin): Call<UsuarioTokenDto>
 }
