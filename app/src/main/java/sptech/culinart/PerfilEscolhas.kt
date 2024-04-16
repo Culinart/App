04a84a591dee727f5daf2fff14cf5003b1d67d05
Calebe-Marques <calebe.reboucas@sptech.school>
2024-04-16 15:23:20 -0300

    feat: tela de perfil escolhas finalizado

--- app/src/main/java/sptech/culinart/PerfilEscolhas.kt
+++ app/src/main/java/sptech/culinart/PerfilEscolhas.kt
@@ -3,12 +3,33 @@ package sptech.culinart
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 import sptech.culinart.ui.theme.CulinartTheme
 
 class PerfilEscolhas : ComponentActivity() {
@@ -17,7 +38,10 @@ class PerfilEscolhas : ComponentActivity() {
         setContent {
             CulinartTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     TelaPerfilEscolhas("Android")
                 }
             }
@@ -26,14 +50,142 @@ class PerfilEscolhas : ComponentActivity() {
 }
 
 @Composable
-fun TelaPerfilEscolhas(name: String, modifier: Modifier = Modifier){
+fun TelaPerfilEscolhas(name: String, modifier: Modifier = Modifier) {
+    ComponenteHader(name = "Android", modifier = Modifier.zIndex(99f))
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 70.dp)
+            .zIndex(1f),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(.8f),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Perfil",
+                style = TextStyle(
+                    Color(4, 93, 83),
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 32.sp
+                )
+            )
 
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Text(
+                text = "Nome do Usuário",
+                style = TextStyle(
+                    Color(4, 93, 83),
+                    fontWeight = FontWeight.Light,
+                    fontSize = 24.sp
+                )
+            )
+
+            Spacer(modifier = Modifier.height(15.dp))
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(1.dp)
+                    .background(Color(android.graphics.Color.parseColor("#C1CECD")))
+            )
+
+            Spacer(modifier = Modifier.height(55.dp))
+
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(46, 196, 182),
+                    contentColor = Color.White
+                )
+            ) {
+                Text(
+                    text = "Informações Pessoais",
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp
+                    )
+                )
+            }
+
+            Spacer(modifier = Modifier.height(45.dp))
+
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(46, 196, 182),
+                    contentColor = Color.White
+                )
+            ) {
+                Text(
+                    text = "Senha e Autenticação",
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp
+                    )
+                )
+            }
+
+            Spacer(modifier = Modifier.height(45.dp))
+
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(46, 196, 182),
+                    contentColor = Color.White
+                )
+            ) {
+                Text(
+                    text = "Meus Endereços",
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp
+                    )
+                )
+            }
+
+            Spacer(modifier = Modifier.height(45.dp))
+
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(46, 196, 182),
+                    contentColor = Color.White
+                )
+            ) {
+                Text(
+                    text = "Pagamento",
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp
+                    )
+                )
+            }
+
+            Spacer(modifier = Modifier.height(150.dp))
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun TelaPerfilEscolhasPreview() {
     CulinartTheme {
-        TelaCadastro("Android")
+        TelaPerfilEscolhas("Android")
     }
 }
