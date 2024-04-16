61e59f505a50728a92bedb060aadc41936c23016
Calebe-Marques <calebe.reboucas@sptech.school>
2024-04-16 19:25:06 -0300

    refactor: remoção de comentario desnecessário

--- app/src/main/java/sptech/culinart/PerfilEscolhas.kt
+++ app/src/main/java/sptech/culinart/PerfilEscolhas.kt
@@ -37,7 +37,6 @@ class PerfilEscolhas : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CulinartTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
